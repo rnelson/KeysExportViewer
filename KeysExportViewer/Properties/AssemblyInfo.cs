375da3d58fa4bf3b93d409dfc37811c5925856a5
Ross Nelson <ross.nelson@gmail.com>
2020-10-04 15:56:53 -0500

    I think it's safe to go to version 1.0 now

--- KeysExportViewer/Properties/AssemblyInfo.cs
+++ KeysExportViewer/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Resources;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Ross Nelson")]
 [assembly: AssemblyProduct("KeysExport Viewer")]
-[assembly: AssemblyCopyright("Copyright © 2013 Ross Nelson")]
+[assembly: AssemblyCopyright("Copyright © 2013-2020 Ross Nelson")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -33,8 +33,8 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.2.0")]
-[assembly: AssemblyFileVersion("0.0.2.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
 
 [assembly: NeutralResourcesLanguageAttribute("en")]
 [assembly: CLSCompliant(true)]
